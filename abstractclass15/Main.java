c8847bcd2fd99647fcfe53ba7438380e7cf09d7f
RakhmanovAziz <124004338+RakhmanovAziz@users.noreply.github.com>
2023-06-19 04:07:53 +0500

    Add files via upload

---
+++ abstractclass15/Main.java
@@ -0,0 +1,12 @@
+package org.zadacha31.abstractclass15;
+
+public class Main {
+    public static void main(String[] args) {
+        linearFunction linear = new linearFunction();
+        quadraticFunction quadratic = new quadraticFunction();
+
+        linear.calculateFunction();
+        quadratic.calculateFunction();
+
+    }
+}
---
+++ abstractclass15/functionConstructor.java
@@ -0,0 +1,5 @@
+package org.zadacha31.abstractclass15;
+
+public abstract class functionConstructor {
+    public abstract void calculateFunction();
+}
